06244c14d1b9d86bb29972e180265084dc5a5749
Max Sokolich <sokolich@udel.edu>
2023-08-09 11:00:27 -0400

    ready for linux test

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from PyQt5.QtWidgets import QApplication
 import sys
 from classes.gui_functions import MainWindow
 
-# try blur thing z
+#test mask speed increase and Linux
+#Remake ui for Linux machine ( make it so it can resize)
+#Convert blur into z position
+#add slots for field values
+# add exposure spinbox
+# add fps spin box? 
+# add objective spin box
+# still try and activily adjust window size
+# maybe add robot list
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
