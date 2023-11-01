2c7e5ad36b172e7c6282aba43b4783e6f9ea3009
DasLab325 <DasLab325@MEEG-DMO9472H0Q.win.udel.edu>
2023-11-01 11:09:47 -0400

    white balance true

--- classes/gui_functions.py
+++ classes/gui_functions.py
@@ -670,7 +670,7 @@ class MainWindow(QtWidgets.QMainWindow):
         if self.videopath == 0:
             try:
                 self.cap  = EasyPySpin.VideoCapture(0)
-                self.cap.set(cv2.CAP_PROP_AUTO_WB, False)
+                self.cap.set(cv2.CAP_PROP_AUTO_WB, True)
             except Exception:
                 self.cap  = cv2.VideoCapture(0) 
                 self.tbprint("No EasyPySpin Camera Available")
